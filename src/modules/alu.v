99655de82c463ead0f7d981db4e5a4aeb5489e63
riancmd <rian.dssantos@gmail.com>
2025-03-16 19:23:32 -0300

    Handling 2's complement in ALU

--- src/modules/alu.v
+++ src/modules/alu.v
@@ -2,12 +2,17 @@ module alu(input [2:0] op, //operation code
 			  input [7:0] r1, r2, //operands
 			  input [2:0] s, //size of the matrix
 			  output [7:0] reg outr); //output result
+			  
+	reg [7:0] negr2, r2f;
+	
+	assign negbb = ~r2;
+	assign r2comp = negbb + 1; //handling 2's complement
 			
 	always@(*)
 	begin
 		case (op)
 			3'b000: //addM (add matrices)
-			3'b001: //subM (subtract matrices)
+			3'b001: assign outr = adder(r1,r2comp); //subM (subtract matrices)
 			3'b010: //multM (multiply matrices)
 			3'b011: //multMR (multiply by real)
 			3'b100: //detM (determinant of matrix)
