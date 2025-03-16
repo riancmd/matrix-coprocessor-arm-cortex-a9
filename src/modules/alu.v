39b20b8dc127fb88cc6efb0c10853889fa6b75eb
riancmd <rian.dssantos@gmail.com>
2025-03-16 19:13:15 -0300

    Added ALU module

---
+++ src/modules/alu.v
@@ -0,0 +1,20 @@
+module alu(input [2:0] op, //operation code
+			  input [7:0] r1, r2, //operands
+			  input [2:0] s, //size of the matrix
+			  output [7:0] reg outr); //output result
+			
+	always@(*)
+	begin
+		case (op)
+			3'b000: //addM (add matrices)
+			3'b001: //subM (subtract matrices)
+			3'b010: //multM (multiply matrices)
+			3'b011: //multMR (multiply by real)
+			3'b100: //detM (determinant of matrix)
+			3'b101: //transM (transpose matrix)
+			3'b110: //oppM (opposite matrix)
+			3'b111: // reset
+		endcase
+	end
+
+endmodule
