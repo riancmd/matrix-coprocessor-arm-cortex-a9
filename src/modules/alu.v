427790e71408fdae78d56e47aafa062e9942b82d
riancmd <rian.dssantos@gmail.com>
2025-03-21 12:00:48 -0300

    minor fixes: ALU

--- src/modules/alu.v
+++ src/modules/alu.v
@@ -1,34 +1,40 @@
 module alu(input [2:0] op, //operation code
 			  input signed [7:0] r1, r2, //operands
 			  input [2:0] s, //size of the matrix
-			  output signed [7:0] reg outr); //output result
+			  output reg signed [7:0] outr); //output result
 			  
 	reg signed [7:0] a, b;
 	//reg [7:0] negr2, r2f;
 	
-	assign a = r1;
-	assign b = r2;
-	
 	localparam [2:0]
-		3'b000: addM //(add matrices)
-		3'b001: subM //(subtract matrices)
-		3'b010: multM //(multiply matrices)
-		3'b011: multMR //(multiply by real)
-		3'b100: detM //(determinant of matrix)
-		3'b101: transM //(transpose matrix)
-		3'b110: oppM //(opposite matrix)
-		3'b111: rst
+		addM = 3'b000, //(add matrices)
+		subM = 3'b001, //(subtract matrices)
+		multM = 3'b010, //(multiply matrices)
+		multMR = 3'b011, //(multiply by real)
+		detM = 3'b100, //(determinant of matrix)
+		transM = 3'b101, //(transpose matrix)
+		oppM = 3'b110, //(opposite matrix)
+		rst = 3'b111; //reset
 	
 	//assign negbb = ~r2;
 	//assign r2comp = negbb + 1; //handling 2's complement
 			
-	assign outr = (op == addM) ? a + b :
-					  (op == subM) ? a - b:
-					  (op == multM) ? a * b:
-					  (op == multMR) ? a * b:
-					  (op == detM) ? 8'b00000001:
-					  (op == transM) ? a * b:
-					  (op == oppM) ? a * b:
-					  (op == rst) ? 1'b1: -1'b1;
+	always @(*) begin
+		a = r1;
+		b = r2;
+		
+		case (op)
+			addM : outr = a + b;
+			subM : outr = a - b;
+			multM : outr = a * b;
+			multMR : outr = a * b;
+			detM : outr = 8'b00000001;
+			transM : outr = a * b;
+			oppM : outr = a * b;
+			rst : outr = 1'b1;
+			default : outr = -1'b1;
+		endcase
+	end
+
 
 endmodule
