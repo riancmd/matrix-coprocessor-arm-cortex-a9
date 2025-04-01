e16a854961dc4b5d2ce5062396ccea4d46b2e86c
riancmd <rian.dssantos@gmail.com>
2025-04-01 10:25:13 -0300

    adderTB

--- src/modules/add_M.v
+++ src/modules/add_M.v
@@ -9,17 +9,21 @@ module add_M(input clk, //sinal de clock
 	
 	always@(posedge clk or posedge rst) begin
 		if(rst) begin //se houver sinal de reset
-			ovf = 0; 
+			ovf = 1'b0; 
 			temp_m = 45'b0;			
 		end
 		else begin //nõ
-			temp_m[44:36] = {1'b0, m1[39:32]} + {1'b0, m2[39:32]}; //primeira soma
-			temp_m[34:27] = {1'b0, m1[31:24]} + {1'b0, m2[31:24]}; //segunda soma
-			temp_m[25:18] = {1'b0, m1[23:16]} + {1'b0, m2[23:16]}; //terceira soma
-			temp_m[16:9]  = {1'b0, m1[15:8]}  + {1'b0, m2[15:8]};  //quarta soma
-			temp_m[7:0]   = {1'b0, m1[7:0]}   + {1'b0, m2[7:0]};   //quinta soma
+			temp_m[44:36] = {m1[39], m1[39:32]} + {1'b0, m2[39:32]}; //primeira soma
+			temp_m[34:27] = {m1[31], m1[31:24]} + {1'b0, m2[31:24]}; //segunda soma
+			temp_m[25:18] = {m1[23], m1[23:16]} + {1'b0, m2[23:16]}; //terceira soma
+			temp_m[16:9]  = {m1[15], m1[15:8]}  + {1'b0, m2[15:8]};  //quarta soma
+			temp_m[7:0]   = {m1[7], m1[7:0]}   + {1'b0, m2[7:0]};   //quinta soma
 
-			ovf = temp_m[8] || temp_m[17] || temp_m[26] || temp_m[35] || temp_m[44]; //verifica overflow no MSB de cada número
+			ovf = (temp_m[44] != temp_m[43]) ||  // Overflow no elemento 0
+          (temp_m[35] != temp_m[34]) ||  // Overflow no elemento 1
+          (temp_m[26] != temp_m[25]) ||  // Overflow no elemento 2
+          (temp_m[17] != temp_m[16]) ||  // Overflow no elemento 3
+          (temp_m[8]  != temp_m[7]);     // Overflow no elemento 4
 		end
 		
 		m_out = {temp_m[43:36], temp_m[34:27], temp_m[25:18], temp_m[16:9], temp_m[7:0]}; //manda linha para saída
