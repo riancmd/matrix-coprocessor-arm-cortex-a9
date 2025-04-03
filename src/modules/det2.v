7cf394dcacd0566b01aa686afcdac464b255df51
riancmd <rian.dssantos@gmail.com>
2025-04-03 16:49:21 -0300

    det2

--- src/modules/det2.v
+++ src/modules/det2.v
@@ -1,11 +1,21 @@
-module Determinante2x2 (
+module det2 (
     input signed [15:0] l1, l2, //duas linhas de 16bits cada, sendo 2 números
     input rst, //sinal de reset
-    output reg signed [15:0] det // Saída 16 bits com sinal
+    output reg signed [7:0] det // Saída 16 bits com sinal
 );
 
+    reg signed [15:0] temp_det; // caso de overflow
+
 always @(*) begin
-    det = (l1[15:8] * l2[7:0]) - (l2[15:8] * l1[7:0]); //
+    if (rst) begin
+        temp_det = 16'b0;
+    end
+
+    else begin
+        temp_det = (l1[15:8] * l2[7:0]) - (l2[15:8] * l1[7:0]);
+    end
+
+    det = temp_det;
 end
 
 endmodule
