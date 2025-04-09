25eafe2e401bd8f3a0c1e64106d83295c08da266
riancmd <rian.dssantos@gmail.com>
2025-04-09 11:23:23 -0300

    minor fixes

--- src/modules/det5.v
+++ src/modules/det5.v
@@ -68,10 +68,10 @@ module det5(
 			.matrix({matrix[159:152], matrix[143:112], matrix[103:72], matrix[63:32], matrix[23:0]}), .rst(rst), .det(det2), .ovf(ovf_det2)
 	  );
 	  det4 matriz3(
-			.matrix({matrix[159:144], matrix[135:104], matrix[95:64], matrix[56:24], matrix[15:0]}), .rst(rst), .det(det3), .ovf(ovf_det3)
+			.matrix({matrix[159:144], matrix[135:104], matrix[95:64], matrix[55:24], matrix[15:0]}), .rst(rst), .det(det3), .ovf(ovf_det3)
 	  );
 	  det4 matriz4(
-			.matrix({matrix[159:136], matrix[127:96], matrix[87:56], matrix[47:15], matrix[7:0]}), .rst(rst), .det(det4), .ovf(ovf_det4)
+			.matrix({matrix[159:136], matrix[127:96], matrix[87:56], matrix[47:16], matrix[7:0]}), .rst(rst), .det(det4), .ovf(ovf_det4)
 	  );
 	  det4 matriz5(
 			.matrix({matrix[159:128], matrix[119:88], matrix[79:48], matrix[39:8]}), .rst(rst), .det(det5), .ovf(ovf_det5)
@@ -79,6 +79,26 @@ module det5(
 
     //cálculo de determinante da matriz 4x4
     always @(*) begin
+
+        $display("det1; %d", det1);
+        $display("det2; %d", det2);
+        $display("det3; %d", det3);
+        $display("det4; %d", det4);
+        $display("det5; %d", det5);
+        $display("ovf1; %d", ovf1);
+        $display("ovf2; %d", ovf2);
+        $display("ovf3; %d", ovf3);
+        $display("ovf4; %d", ovf4);
+        $display("ovf5; %d", ovf5);
+        $display("ovfdet1; %d", ovf_det1);
+        $display("ovfdet2; %d", ovf_det2);
+        $display("ovfdet3; %d", ovf_det3);
+        $display("ovfdet4; %d", ovf_det4);
+        $display("ovfdet5; %d", ovf_det5);
+        $display("det2 igual -128; %d", (det2 == -128));
+        $display("det4 igual -128; %d", (det4 == -128));
+        $display("tempdet maior que 127; %d", (temp_det > 127));
+        $display("tempdet menor que -128; %d", (temp_det < -128));
         temp_det = det1 + det2 + det3 + det4 + det5;
         det = temp_det[7:0]; 
         ovf = (ovf_det1 || ovf_det2 || ovf_det3 || ovf_det4 || ovf_det5 || det2 == -128 || det4 == -128 || temp_det > 127 || temp_det < -128 ||
